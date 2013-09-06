9b0ff7c8cdbcb5acce112193539c32c6ba535db8
lukepinnerau <lukepinnerau@b8bf4c51-e9db-6fcb-c41f-325b9d4ae042>
2013-09-06 00:09:45 +0000

    Don't need zip file

--- setup.cmd
+++ setup.cmd
@@ -1,4 +1,4 @@
-python setup.py sdist  --formats=gztar,zip
+python setup.py sdist  --formats=gztar
 python setup.py bdist --format=wininst
 "C:\Python27\ArcGISx6410.1\python.exe" setup.py bdist --format=wininst
 rmdir /S /Q build
