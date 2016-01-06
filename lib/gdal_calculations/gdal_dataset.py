4a85882c8d4a8fceebe7763568535d3055830928
lpinner <lpinner@users.noreply.github.com>
2016-01-06 14:35:25 +1100

    Fix "'Band' object has no attribute 'RasterXSize'" when Env.tiled = False

--- lib/gdal_calculations/gdal_dataset.py
+++ lib/gdal_calculations/gdal_dataset.py
@@ -501,7 +501,8 @@ class Band(RasterLike):
            through to the underlying GDALBand/ndarray objects'''
         if attr=='dtype':raise TypeError #so numpy ufuncs work
         #if attr in ('dtype','__array__struct__'):raise TypeError #so numpy ufuncs work
-        if attr in dir(gdal.Band):return getattr(self._band, attr)
+        elif attr in dir(gdal.Band):return getattr(self._band, attr)
+        elif attr in dir(gdal.Dataset):return getattr(self.dataset, attr)
         elif attr in dir(np.ndarray):
             if callable(getattr(np.ndarray,attr)):return self.__ndarraymethod__(attr)
             else:return self.__ndarrayattribute__(attr)
